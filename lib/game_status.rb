f90ef8810b9e4e57e3f4c07d72ade5893fc0e3b8
luisandra abreu duran <abreuluisandra@gmail.com>
2020-06-16 00:26:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
+WIN_COMBINATIONS() = [
   [0,1,2],  #TOP ROW
   [3,4,5],  #MIDDLE row
   [6,7,8],
